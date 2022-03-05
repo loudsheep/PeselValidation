12673b48f6ea18fe44ed4ff61ee43462b7d554c3
loudsheep <rochmykietow@gmail.com>
2022-03-05 08:38:03 +0100

    App is working as intended

--- PeselValidation/MainPage.xaml.cs
+++ PeselValidation/MainPage.xaml.cs
@@ -14,5 +14,53 @@ namespace PeselValidation
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string pesel = inputPESEL.Text;
+            bool check = CheckPESEL(pesel);
+            string message = "PESEL ";
+            message += check ? "" : "nie ";
+            message += "jest poprawny";
+
+            DisplayAlert("", message, "Anuluj");
+        }
+
+        private static int[] weigths = new int[] {1, 3, 7, 9};
+        private static bool CheckPESEL(string pesel)
+        {
+            if (pesel.Length != 11) 
+            {
+                return false; 
+            }
+
+            int sum = 0;
+            for(int i=0; i<pesel.Length - 1; i++)
+            {
+                if(Char.IsDigit(pesel[i]))
+                {
+                    int n = pesel[i] - '0';
+                    sum += n * weigths[i % weigths.Length];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if (!Char.IsDigit(pesel[10])) 
+            {
+                return false; 
+            }
+
+            sum = sum % 10;
+            sum = 10 - sum;
+            sum = sum == 10 ? 0 : sum;
+
+            int last = pesel[10] - '0';
+
+            if (last != sum) return false;
+
+            return true;
+        }
     }
 }
